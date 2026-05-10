12f67e221a614d4f1aff2cb214df0225a9af200b
Phuc Thinh <25023404@vnu.edu.vn>
2026-05-10 21:56:34 +0700

    Add Utils with checkstyle violations

---
+++ src/main/java/com/review/Utils.java
@@ -0,0 +1,10 @@
+package com.review;
+
+public class Utils {
+    // Cố ý đặt tên biến sai chuẩn (chữ hoa đầu)
+    private String MyName;
+
+    public void PrintName() {    // Tên phương thức viết hoa
+        System.out.println(MyName);
+    }
+}
